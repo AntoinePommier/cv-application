56c2b5dc50adcf9d438e3ddde5eebf2b8f74fd8b
Antoine Pommier <140323098+AntoinePommier@users.noreply.github.com>
2026-03-01 13:38:40 +0900

    Personal info form logic done + some styling

--- src/components/CvForm/PersonalInfoForm.jsx
+++ src/components/CvForm/PersonalInfoForm.jsx
@@ -3,6 +3,7 @@ function PersonalInfoForm({ cvData, updatePersonal }) {
 
   return (
     <form className="personal-info-form">
+      <h1>Personal Details</h1>
       <div className="input-div">
         <label htmlFor="full-name">Full Name</label>
         <input type="text" id="full-name" placeholder="First and last name" value={cvData.personal.fullName} onChange={(e) => updatePersonal("fullName", e.target.value)} />
@@ -10,6 +11,22 @@ function PersonalInfoForm({ cvData, updatePersonal }) {
       <div className="input-div">
         <label htmlFor="email">Email</label>
         <input type="email" id="email" placeholder="Email" value={cvData.personal.email} onChange={(e) => updatePersonal("email", e.target.value)} />
+      </div>
+      <div className="input-div">
+        <label htmlFor="phone">Phone</label>
+        <input type="tel" id="phone" placeholder="Phone" value={cvData.personal.phone} onChange={(e) => updatePersonal("phone", e.target.value)} />
+      </div>
+      <div className="input-div">
+        <label htmlFor="location">Location</label>
+        <input type="text" id="location" placeholder="Location" value={cvData.personal.location} onChange={(e) => updatePersonal("location", e.target.value)} />
+      </div>
+      <div className="input-div">
+        <label htmlFor="linkedin">Linkedin</label>
+        <input type="text" id="linkedin" placeholder="Linkedin" value={cvData.personal.linkedin} onChange={(e) => updatePersonal("linkedin", e.target.value)} />
+      </div>
+      <div className="input-div">
+        <label htmlFor="website">Website</label>
+        <input type="text" id="website" placeholder="Website" value={cvData.personal.website} onChange={(e) => updatePersonal("website", e.target.value)} />
       </div>
     </form>
   )
